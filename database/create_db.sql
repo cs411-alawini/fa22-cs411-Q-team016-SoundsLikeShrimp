8edaadfc5b56770b72b903de2a86207b9d239acc
cookiefu0225 <cfyang2@illinois.edu>
2022-11-07 11:38:12 -0600

    Database schema update

--- database/create_db.sql
+++ database/create_db.sql
@@ -32,7 +32,7 @@ CREATE TABLE Reservation(
     checkout_date   INT,
     duration        INT,
 
-    FOREIGN KEY(email) REFERENCES User(email) ON UPDATE CASCADE,
+    FOREIGN KEY(email) REFERENCES User(email) ON UPDATE CASCADE ON DELETE CASCADE,
     FOREIGN KEY(room_number) REFERENCES Room(room_number) ON UPDATE CASCADE,
     CONSTRAINT PRIMARY KEY(reservation_id, email, room_number)
 );
@@ -41,4 +41,46 @@ CREATE TABLE Service(
     service_id  INT PRIMARY KEY,
     price       INT NOT NULL,
     type        INT CHECK(type >= 1)
+);
+
+-- Mobiles foreign key emp_id cannot be NULL, but Employee foreign key mob_id can --
+CREATE TABLE Employee(
+    emp_id      INT PRIMARY KEY,
+    title       VARCHAR(255) NOT NULL,
+    salary      INT NOT NULL,
+    mob_id      INT
+);
+
+CREATE TABLE Mobiles(
+    mob_id          INT PRIMARY KEY,
+    type            INT,
+    emp_id          INT NOT NULL,
+    accomodation    INT NOT NULL,
+
+    FOREIGN KEY(emp_id) REFERENCES Employee(emp_id) ON UPDATE CASCADE
+);
+
+CREATE TABLE Charter(
+    chater_id       INT,
+    mob_id          INT,
+    email           VARCHAR(255),
+    destination     VARCHAR(255),
+    rent_date       DATE NOT NULL,
+    time            TIME NOT NULL,
+    price           INT NOT NULL,
+
+    FOREIGN KEY(mob_id) REFERENCES Mobiles(mob_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(email) REFERENCES User(email) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT PRIMARY KEY(chater_id, mob_id, email)
+);
+
+CREATE TABLE Request(
+    time            TIME,
+    room_number     INT REFERENCES Room(room_number)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE,
+    service_id      INT REFERENCES Service(service_id)
+                    ON UPDATE CASCADE,
+
+    CONSTRAINT PRIMARY KEY(time, room_number, service_id)
 )
